65b6c641477529549c87e064aa841bc551130894
kellywinzer <bignateswife@gmail.com>
2017-12-05 13:22:13 -0600

    Trouble with the new-post form validation.

--- main.py
+++ main.py
@@ -25,17 +25,27 @@ def index():
     posts = Blog.query.all()
     return render_template('blog.html', title="Build a Blog", posts=posts)
 
-@app.route('/new-post', methods=['POST'])
+@app.route('/new-post', methods=['POST', 'GET'])
 def new_post():
-    
+
+    title_error = ''
+    body_error = ''
+
     if request.method == 'POST':
         post_title = request.form['post_title']
         post_body = request.form['post_body']
-        new_post = Blog(post_title, post_body)
-        db.session.add(new_post)
+        new = Blog(post_title, post_body)
+        db.session.add(new)
         db.session.commit()
+        if post_title == '':
+            title_error = 'Please enter a Post Title.'
+        elif body_error == '':
+            body_error = 'Please enter some content.'
+        else:
+            return redirect('/blog')
 
-    return render_template('new-post.html', title="New Post")
+    return render_template('new-post.html', title="New Post", title_error=title_error,
+                body_error=body_error)
 
 if __name__ == '__main__':
     app.run()
