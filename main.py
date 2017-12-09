3eb7d4bbc0497e665f1d1b923fa76e2ee011fdaf
kellywinzer <bignateswife@gmail.com>
2017-12-09 12:44:36 -0600

    Project finished, meets all requirements.

--- main.py
+++ main.py
@@ -45,22 +45,18 @@ def new_post():
             db.session.add(new)
             db.session.commit()
 
-            return redirect('/single-post?id={0}&post_title={1}&post_body={2}'.format(new.id, new.post_title, new.post_body))
+            return redirect('/single-post?id={0}'.format(new.id))
 
     return render_template('new-post.html', title="New Post", title_error=title_error,
                 body_error=body_error, post_title=post_title, post_body=post_body)
 
-@app.route('/single-post')
+@app.route('/single-post', methods=['GET'])
 def single_post():
 
-    post_title = request.args.get('post_title')
-    post_body = request.args.get('post_body')
+    retrieved_id = request.args.get('id')
+    posts = db.session.query(Blog.post_title, Blog.post_body).filter_by(id=retrieved_id)
 
-    id = Blog.query.get('id')
-    # post_title = Blog.query.get('post_title')
-    # post_body = Blog.query.get('post_body')
-
-    return render_template('single-post.html', title="Single Post", post_title=post_title, post_body=post_body)
+    return render_template('single-post.html', title="Single Post", posts=posts)
 
 if __name__ == '__main__':
     app.run()
